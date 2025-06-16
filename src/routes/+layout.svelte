ee942890595c9e8d2019df1172d047c985e463ac
Andrew <andrewianstewart@gmail.com>
2025-06-16 19:42:47 -0400

    Fix mobile view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   <link rel="alternate" type="application/rss+xml" title="src.rip RSS Feed" href="/rss.xml" />
 </svelte:head>
 
-<div class="max-w-3xl mx-auto px-4 py-6">
+<div class="w-full max-w-3xl mx-auto px-4 py-6">
   <header class="mb-8">
     <h1 class="text-lg font-normal mb-2">
       <a href="/">blog.src.rip</a>
