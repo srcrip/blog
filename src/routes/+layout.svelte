82ae6b9aa0309588cb0cadb864ec0be18fe8e532
Andrew <andrewianstewart@gmail.com>
2025-06-16 19:19:48 -0400

    Add proper link tag to rss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import '../main.css'
 </script>
 
+<svelte:head>
+  <link rel="alternate" type="application/rss+xml" title="src.rip RSS Feed" href="/rss.xml" />
+</svelte:head>
+
 <div class="max-w-3xl mx-auto px-4 py-6">
   <header class="mb-8">
     <h1 class="text-lg font-normal mb-2">
@@ -11,7 +15,8 @@
       <a href="/" class="mr-4">blog</a>
       <a href="/consulting" class="mr-4">consulting</a>
       <a href="https://github.com/srcrip" class="mr-4">github</a>
-      <a href="https://twitter.com/src_rip">twitter</a>
+      <a href="https://twitter.com/src_rip" class="mr-4">twitter</a>
+      <a href="/rss.xml" class="mr-4">rss</a>
     </nav>
   </header>
   
